7b3749f723347ad27f864a041bbd3ab16f56c3fd
Jon Fus <jonfus123@hotmail.com>
2020-06-08 22:18:37 -0400

    Added luafilesystem

--- build.lua
+++ build.lua
@@ -1,7 +1,23 @@
 local config = require("config")
+local lfs = require("lfs")
 
+
+function hash(path)
+    for file in lfs.dir(path) do
+        if (file ~= nil and file ~= "." and file ~= "..") then
+            local attr = lfs.attributes(path .. "/" .. file)
+
+            if (attr.mode == "directory") then
+                print(file .. " = dir")
+            else
+                print(file .. " = file")
+            end
+        end
+    end
+end
 for i = 1,#config.builds do
     local cmd = string.format("%s -quit -batchmode -logFile stdout.log -projectPath %s -executeMethod BuildScript.%s", config.unityPath, config.projectPath, config.builds[i])
     print(cmd)
-    os.execute(cmd)
+    --os.execute(cmd)
+    hash("F:/dev/Ballers/builds/Windows64/")
 end
